d6cd0d97c97cf2fba9e045b575726f11ebed4804
bruno-bueno <bbueno1122@gmail.com>
2023-08-14 22:07:22 -0300

    telas api funcionando

--- script.js
+++ script.js
@@ -4,34 +4,32 @@ const noticiaGrandeContainer = document.querySelector('#noticiaGrande');
 const noticiaPequenaContainer = document.querySelector('#noticiaPequena');
 let ativado=false;
 let erro=false;
+let index=0;
 
 async function getPosts(){
-    let index=0;
-    do{
-        try{
-            const response = await fetch(url+index);
-            //console.log(response);
-            const data= await response.json();
-            //console.log(data)
-            await espalharPosts(index,data.thumbImage, data.thumbImageAltText, data.title)
-            index++;
-        }catch{
-            erro=true
-        }
-    }while(!erro);
+    try{
+        const response = await fetch(url+index);
+        const data= await response.json();
+        console.log(data);
+        await espalharPosts(index,data.thumbImage, data.thumbImageAltText, data.title, data.description)
+        index++;
+        getPosts();
+    }catch{
+    }
+
 }
 
-async function espalharPosts(index,img,alt,title){
+async function espalharPosts(index,img,alt,title,description){
     console.log(index)
     if(index<2){
         await criarCarrosel(img,alt,title);
         console.log("criar carrossel")
     }else{
         if(index%2==0){
-            criarNoticiaGrande();
+            criarNoticiaGrande(img,alt,title,description);
             console.log("noticia grande");
         }else{
-            criarNoticiaPequena();
+            criarNoticiaPequena(img,alt,title);
             console.log("noticia pequena");
         }
     }
@@ -53,7 +51,7 @@ function criarCarrosel(img,alt,title){
     imagem.alt = alt;
  
     const h2 = document.createElement('h2');
-    h2.className = 'fs-4';
+    h2.className = 'fs-3';
     h2.textContent = title;
  
     div.appendChild(imagem);
@@ -64,12 +62,68 @@ function criarCarrosel(img,alt,title){
 }
 
 
-function criarNoticiaGrande(){
+function criarNoticiaGrande(img,alt,title,description){
+    const card = document.createElement("div");
+    card.className = "card mb-3 w-100 noticiaGrande mt-5";
 
+    const row = document.createElement("div");
+    row.className = "row g-0";
+
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "col-md-4";
+
+    const imagem = document.createElement("img");
+    imagem.className = "img-fluid rounded-start";
+    imagem.src = "https://api-rest-post-diegocandido.herokuapp.com"+img;
+    imagem.alt = alt;
+
+    imgDiv.appendChild(imagem);
+
+    const col = document.createElement("div");
+    col.className = "col-md-8";
+
+    const corpo = document.createElement("div");
+    corpo.className = "card-body";
+
+    const titulo = document.createElement("h5");
+    titulo.className = "card-title fs-5";
+    titulo.textContent = title;
+
+    const descricao = document.createElement("p");
+    descricao.className = "card-text fs-6";
+    descricao.textContent = description;
+
+    corpo.appendChild(titulo);
+    corpo.appendChild(descricao);
+
+    col.appendChild(corpo);
+
+    row.appendChild(imgDiv);
+    row.appendChild(col);
+
+    card.appendChild(row);
+
+    noticiaGrandeContainer.appendChild(card);
 }
 
-function criarNoticiaPequena(){
+function criarNoticiaPequena(img,alt,title){
+    const card = document.createElement("div");
+    card.className = "card mt-5 noticiaPequena";
 
+    const imagem = document.createElement("img");
+    imagem.className = "img-fluid rounded ";
+    imagem.src = "https://api-rest-post-diegocandido.herokuapp.com"+img;
+    imagem.alt = alt;
+
+    const titleElement = document.createElement("h5");
+    titleElement.className = "card-title fs-5 px-2 rounded";
+    titleElement.textContent = title; 
+
+    card.appendChild(imagem);
+
+    card.appendChild(titleElement);
+
+    noticiaPequenaContainer.appendChild(card);
 }
 
 getPosts();
